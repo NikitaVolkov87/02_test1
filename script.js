9c9823767933107d01d727d368515178028f2882
Nikita Volkov <2545442@gmail.com>
2019-11-16 10:47:03 +0200

    modify promisified ajax

--- script.js
+++ script.js
@@ -7,26 +7,22 @@ document.addEventListener("DOMContentLoaded",function() {
             console.log(formInputs[i].value);
             dataToSend[formInputs[i].name] = formInputs[i].value;
         }
-        const promise = new Promise((resolve) => {
-            // resolve(sendAjax('processForm.php', dataToSend));
-            var xmlhttp = new XMLHttpRequest();
-            xmlhttp.open("POST", '/processForm.php');
-            xmlhttp.setRequestHeader("Content-Type", "application/json");
-            xmlhttp.send(JSON.stringify(dataToSend));
-            xmlhttp.onload = () => {
-                resolve( JSON.parse( xmlhttp.response ));
-            };
+        const promise = new Promise(function(resolve) {
+            sendAjax('processForm.php', dataToSend, resolve);
         });
-        promise.then( result => console.log('promise result is: ', result));
+        promise.then( function(result) {
+            alert(result.body.name);
+            console.log('promise result is: ', result);
+        });
     });
 
-    function sendAjax(url, data) {
-        var xmlhttp = new XMLHttpRequest();
+    function sendAjax(url, data, resolve) {
+        const xmlhttp = new XMLHttpRequest();
         xmlhttp.open("POST", '/'+url);
         xmlhttp.setRequestHeader("Content-Type", "application/json");
         xmlhttp.send(JSON.stringify(data));
         xmlhttp.onload = function() {
-            return xmlhttp.response;
-        }
+            resolve(JSON.parse(xmlhttp.response));
+        };
     }
 });
