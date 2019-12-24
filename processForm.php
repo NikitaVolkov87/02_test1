c5970075f3440cf82ec9248a8eba4d9c0e4f02a0
Nikita Volkov <2545442@gmail.com>
2019-12-25 01:25:13 +0200

    add php class extends test and node script

--- processForm.php
+++ processForm.php
@@ -1,9 +1,29 @@
 <?php
 
+class One
+{
+    public $test1;
+
+    public function __construct()
+    {
+        $this->test1 = 'ok1';
+    }
+    public function getV()
+    {
+        return $this->test2;
+    }
+}
+
+class Two extends One
+{
+    protected $test2 = 'test2 is ok! =)';
+}
+
 $gotData = json_decode(file_get_contents('php://input'), true);
 $resp = [
     'responce' => 'success',
-    'body' => $gotData
+    'body' => $gotData,
+    'tests' => (new Two())->getV(),
 ];
 
 header('Content-Type: application/json');
